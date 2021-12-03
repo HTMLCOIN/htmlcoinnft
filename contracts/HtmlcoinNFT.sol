0abb6bd1514d7628ed2d8dcfe6af5f423c4d731c
Jaron Rosenau <jaron.rosenau@gmail.com>
2021-12-03 03:38:22 -0800

    fix build issue

--- contracts/HtmlcoinNFT.sol
+++ contracts/HtmlcoinNFT.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "../node_modules/@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract HtmlcoinNFT is ERC1155 {
     constructor() ERC1155("https://info.htmlcoin.com/api/item/{id}.json") {
